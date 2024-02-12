ff87e56eb61f4f13a14f265775678226a1a9f46f
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-02-12 12:30:51 +0200

    Fix Namepsacing

--- lib/decidim/maintainers_toolbox/backports_reporter/csv_report.rb
+++ lib/decidim/maintainers_toolbox/backports_reporter/csv_report.rb
@@ -2,30 +2,32 @@
 
 require_relative "report"
 
-module Decidim::MaintainersToolbox
-  module BackportsReporter
-    class CSVReport < Decidim::MaintainersToolbox::BackportsReporter::Report
-      private
+module Decidim
+  module MaintainersToolbox
+    module BackportsReporter
+      class CSVReport < Decidim::MaintainersToolbox::BackportsReporter::Report
+        private
 
-      def output_head
-        "ID;Title;Backport v#{last_version_number};Backport v#{penultimate_version_number}\n"
-      end
+        def output_head
+          "ID;Title;Backport v#{last_version_number};Backport v#{penultimate_version_number}\n"
+        end
 
-      def output_line(line)
-        output = "#{line[:id]};"
-        output += "#{line[:title]};"
-        output += "#{format_backport(line[:related_issues], "v#{last_version_number}")};"
-        output += "#{format_backport(line[:related_issues], "v#{penultimate_version_number}")}\n"
-        output
-      end
+        def output_line(line)
+          output = "#{line[:id]};"
+          output += "#{line[:title]};"
+          output += "#{format_backport(line[:related_issues], "v#{last_version_number}")};"
+          output += "#{format_backport(line[:related_issues], "v#{penultimate_version_number}")}\n"
+          output
+        end
 
-      def format_backport(related_issues, version)
-        return if related_issues.empty?
+        def format_backport(related_issues, version)
+          return if related_issues.empty?
 
-        pull_request = extract_backport_pull_request_for_version(related_issues, version)
-        return if pull_request.nil?
+          pull_request = extract_backport_pull_request_for_version(related_issues, version)
+          return if pull_request.nil?
 
-        "#{pull_request[:state]}|#{pull_request[:id]}"
+          "#{pull_request[:state]}|#{pull_request[:id]}"
+        end
       end
     end
   end
--- lib/decidim/maintainers_toolbox/github_manager/querier/by_title.rb
+++ lib/decidim/maintainers_toolbox/github_manager/querier/by_title.rb
@@ -2,53 +2,55 @@
 
 require_relative "base"
 
-module Decidim::MaintainersToolbox
-  module GithubManager
-    module Querier
-      # Makes a GET request for the list of Issues or Pull Requests in GitHub.
-      #
-      # @param token [String] token for GitHub authentication
-      # @param title [String] the title that we want to search by
-      # @param state [String] the state of the issue. By default is "open"
-      #
-      # @see https://docs.github.com/en/rest/issues/issues#list-repository-issues GitHub API documentation
-      class ByTitle < Decidim::MaintainersToolbox::GithubManager::Querier::Base
-        def initialize(title:, token:, state: "open")
-          @title = title
-          @token = token
-          @state = state
-        end
-
-        # Makes the GET request and parses the response of an Issue or Pull Request in GitHub
+module Decidim
+  module MaintainersToolbox
+    module GithubManager
+      module Querier
+        # Makes a GET request for the list of Issues or Pull Requests in GitHub.
         #
-        # @return [Hash]
-        def call
-          data = json_response("https://api.github.com/repos/decidim/decidim/issues")
-
-          parse(data)
-        end
-
-        private
-
-        attr_reader :title, :state
-
-        def headers
-          {
-            title: title,
-            state: state,
-            per_page: 100
-          }
-        end
-
-        # Parses the response of an Issue or Pull Request in GitHub
+        # @param token [String] token for GitHub authentication
+        # @param title [String] the title that we want to search by
+        # @param state [String] the state of the issue. By default is "open"
         #
-        # @return [Hash]
-        def parse(metadata)
-          metadata.map do |item|
+        # @see https://docs.github.com/en/rest/issues/issues#list-repository-issues GitHub API documentation
+        class ByTitle < Decidim::MaintainersToolbox::GithubManager::Querier::Base
+          def initialize(title:, token:, state: "open")
+            @title = title
+            @token = token
+            @state = state
+          end
+
+          # Makes the GET request and parses the response of an Issue or Pull Request in GitHub
+          #
+          # @return [Hash]
+          def call
+            data = json_response("https://api.github.com/repos/decidim/decidim/issues")
+
+            parse(data)
+          end
+
+          private
+
+          attr_reader :title, :state
+
+          def headers
             {
-              id: item["number"],
-              title: item["title"]
+              title: title,
+              state: state,
+              per_page: 100
             }
+          end
+
+          # Parses the response of an Issue or Pull Request in GitHub
+          #
+          # @return [Hash]
+          def parse(metadata)
+            metadata.map do |item|
+              {
+                id: item["number"],
+                title: item["title"]
+              }
+            end
           end
         end
       end
