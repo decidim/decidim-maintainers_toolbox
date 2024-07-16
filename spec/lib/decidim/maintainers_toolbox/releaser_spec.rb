b6e8c6eede08efb0d943ce2e6f71253a4fa557bf
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-07-16 09:57:48 +0200

    Use explicit RSpec call in specs

--- spec/lib/decidim/maintainers_toolbox/releaser_spec.rb
+++ spec/lib/decidim/maintainers_toolbox/releaser_spec.rb
@@ -2,7 +2,7 @@
 
 require "decidim/maintainers_toolbox/releaser"
 
-describe Decidim::MaintainersToolbox::Releaser do
+RSpec.describe Decidim::MaintainersToolbox::Releaser do
   subject { described_class.new(token: token, version_type: version_type, exit_with_unstaged_changes: exit_with_unstaged_changes, working_dir: tmp_repository_dir) }
 
   let(:token) { "1234" }
