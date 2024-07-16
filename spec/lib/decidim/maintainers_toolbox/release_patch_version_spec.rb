d060c23037946eb459b3e11dfe26d0f3439a71f1
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-07-16 18:03:37 +0200

    Move #next_version_number_for_patch_release method spec to ReleasePatchVersion spec

--- spec/lib/decidim/maintainers_toolbox/release_patch_version_spec.rb
+++ spec/lib/decidim/maintainers_toolbox/release_patch_version_spec.rb
@@ -16,5 +16,30 @@ RSpec.describe Decidim::MaintainersToolbox::ReleasePatchVersion do
       expect(subject.send(:release_branch)).to eq release_branch
     end
   end
-end
 
+  describe "#next_version_number_for_patch_release" do
+    context "when it is a dev version" do
+      let(:version_number) { "0.1.0.dev" }
+
+      it "returns the correct next version number" do
+        expect { subject.send(:next_version_number_for_patch_release, version_number) }.to raise_error(Decidim::MaintainersToolbox::ReleaserUtils::InvalidVersionTypeError)
+      end
+    end
+
+    context "when it is a release candidate version" do
+      let(:version_number) { "0.1.0.rc1" }
+
+      it "returns first patch number" do
+        expect(subject.send(:next_version_number_for_patch_release, version_number)).to eq "0.1.0"
+      end
+    end
+
+    context "when it is a patch version" do
+      let(:version_number) { "0.1.0" }
+
+      it "raises an error" do
+        expect(subject.send(:next_version_number_for_patch_release, version_number)).to eq "0.1.1"
+      end
+    end
+  end
+end
--- spec/lib/decidim/maintainers_toolbox/releaser_spec.rb
+++ spec/lib/decidim/maintainers_toolbox/releaser_spec.rb
@@ -46,32 +46,6 @@ RSpec.describe Decidim::MaintainersToolbox::Releaser do
     end
   end
 
-  describe "#next_version_number_for_patch_release" do
-    context "when it is a dev version" do
-      let(:version_number) { "0.1.0.dev" }
-
-      it "returns the correct next version number" do
-        expect { subject.send(:next_version_number_for_patch_release, version_number) }.to raise_error(Decidim::MaintainersToolbox::Releaser::InvalidVersionTypeError)
-      end
-    end
-
-    context "when it is a release candidate version" do
-      let(:version_number) { "0.1.0.rc1" }
-
-      it "returns first patch number" do
-        expect(subject.send(:next_version_number_for_patch_release, version_number)).to eq "0.1.0"
-      end
-    end
-
-    context "when it is a patch version" do
-      let(:version_number) { "0.1.0" }
-
-      it "raises an error" do
-        expect(subject.send(:next_version_number_for_patch_release, version_number)).to eq "0.1.1"
-      end
-    end
-  end
-
   describe "#old_version_number" do
     let(:decidim_version) { "0.1.0" }
 
