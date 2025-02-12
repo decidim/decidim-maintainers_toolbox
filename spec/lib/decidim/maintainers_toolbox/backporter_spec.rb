16e3acb329b844c6b88da776747a42507dedbc35
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-02-12 11:53:18 +0100

    Add missing argument from bad git conflict resolution

--- spec/lib/decidim/maintainers_toolbox/backporter_spec.rb
+++ spec/lib/decidim/maintainers_toolbox/backporter_spec.rb
@@ -3,7 +3,7 @@
 require "decidim/maintainers_toolbox/backporter"
 
 RSpec.describe Decidim::MaintainersToolbox::Backporter do
-  subject { described_class.new(token: token, pull_request_id: pull_request_id, version_number: version_number, exit_with_unstaged_changes: exit_with_unstaged_changes) }
+  subject { described_class.new(token: token, pull_request_id: pull_request_id, version_number: version_number, exit_with_unstaged_changes: exit_with_unstaged_changes, with_console: with_console) }
 
   let(:token) { "1234" }
   let(:pull_request_id) { 123 }
